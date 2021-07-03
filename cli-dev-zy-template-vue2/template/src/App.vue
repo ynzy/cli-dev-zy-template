8bf520b3114d123b6d3a945fdcfca83f1246c360
zhangyong <zhangyong@uino.com>
2021-07-03 10:40:59 +0800

    项目模板

---
+++ cli-dev-zy-template-vue2/template/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    vue2 template
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
